9d2f0fe8f6a88987b922d7bc6c56ba32e06ab654
Vladyslav <vladyslav@Vladyslavs-MacBook-Pro.local>
2017-09-06 23:48:05 +0300

    add routes and lost files

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'show' => 'users#show'
+
+  root 'users#index'
+
+  # Эти две строчки добавляют ресурсы для пользователей и вопросов. Ресурс — это
+  # набор путей для управления соответствующей моделью. Посмотрите, что напишет
+  # команда
+  #
+  # rake routes
+  resources :users
+  resources :questions
 end
