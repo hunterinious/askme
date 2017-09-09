6a6069caced3f60e24d96101afe3f1ced1987099
Vladyslav <vladyslav@Vladyslavs-MacBook-Pro.local>
2017-09-09 17:48:43 +0300

    add sessions and modify application.erb.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'show' => 'users#show'
-
   root 'users#index'
 
-  # Эти две строчки добавляют ресурсы для пользователей и вопросов. Ресурс — это
-  # набор путей для управления соответствующей моделью. Посмотрите, что напишет
-  # команда
+  # Ресурс пользователей (экшен destroy не поддерживается)
+  resources :users, except: [:destroy]
+
+  # Ресурс сессий (только три экшена :new, :create, :destroy)
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # Ресурс вопросов (кроме экшенов :show, :new, :index)
+  resources :questions, except: [:show, :new, :index]
+
+  # Синонимы путей — в дополнение к созданным в ресурсах выше.
   #
-  # rake routes
-  resources :users
-  resources :questions
+  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
+  # их удалим.
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
